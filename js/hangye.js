cc94474b9cb9c30cb43073ccd19777df3879fa05
wolf-wh <827240599@qq.com>
2018-01-19 14:11:55 +0800

    da

--- js/hangye.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  console.log($('.dis'),$('.left>ul>li>.qie'))
-    $('.left>ul>li>.qie').each(function (index,ele) {
-    	ele.onclick=function(){
-    		$('.dis').removeClass('d-frist').eq(index).addClass('d-frist');
-    	}
-    })
-})
--- js/index.js
+++ js/index.js
@@ -1,35 +1,36 @@
-$(function () {
-    let a = $(".banner") ,b=$(".dian");
-    let bannerTime = setInterval(move,3000);
-    let n = 1;
-    function move() {
-        n++;
-        if(n == 4){
-            n = 1;
+window.onload = function () {
+    let bei = document.querySelector(".banner");
+    let tu = document.querySelectorAll(".banner>.tu");
+    let dian = document.querySelectorAll(".banc_son");
+    let now = 0;
+    let time = setInterval(t,2000);
+    function t(){
+        if(now == tu.length){
+            now = 0
         }
-        if(n == 3){
-            a.removeClass('banner banner2').addClass('banner1');
-            b.removeClass('d-frist').eq(2).addClass('d-frist');
-        }
-        else if(n == 2){
-            a.removeClass('banner banner1').addClass('banner2');
-            b.removeClass('d-frist').eq(1).addClass('d-frist');
-        }
-        else if (n == 1){
-            a.removeClass('banner2 banner1').addClass('banner');
-            b.removeClass('d-frist').eq(0).addClass('d-frist');
-
-        }
+        tu.forEach(function (ele,index) {
+            ele.style.opacity = 0;
+            dian[index].style.backgroundColor = "#fff";
+        });
+        tu[now].style.opacity = 1;
+        dian[now].style.backgroundColor = "#E1472F";
+        now++;
     }
-    b.each(function(index,ele){
-            ele.onmouseover = function(){
-                clearInterval(bannerTime);
-                n = index;
-                move();
-            }
-            ele.onmouseout = function(){
-                bannerTime =setInterval(move,3000);
-            }
+    bei.onmouseover = function () {
+        clearInterval(time);
     }
-    );
-})
+    bei.onmouseout = function () {
+        time = setInterval(t,2000);
+    }
+    dian.forEach(function (ele,index) {
+        ele.addEventListener('click', function () {
+            tu.forEach(function (ele1,index1) {
+                ele1.style.opacity = 0;
+                dian[index1].style.backgroundColor = "#fff";
+            })
+            tu[index].style.opacity = 1;
+            this.style.backgroundColor = "#E1472F";
+            now = index;
+        })
+    })
+}
